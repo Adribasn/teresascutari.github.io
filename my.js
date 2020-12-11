6e1870883c2faf5677c3864eb82b6df05b05619f
Teresa Scutari <adrien.basyn@outlook.com>
2020-12-11 22:20:11 +0100

    updated js and html

--- my.js
+++ my.js
@@ -1,3 +1,4 @@
+// Smooth scroll
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -12,4 +13,36 @@ $(function() {
       }
     });
   });
-  
+// Submit form
+$(document).ready(function () {
+  $('.submit').click(function (event) {
+    event.preventDefault()
+
+    var name = $('.name').val()
+    var surname = $('.surname').val()
+    var email = $('.email').val()
+    var sujet = $('.sujet').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+
+    if(email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append('<div>Email valide</div>')
+    } else {
+      statusElm.append('<div>Email non valide</div>')
+    }
+
+    if(sujet.length > 2) {
+      statusElm.append('<div>Sujet valide</div>')
+    } else {
+      statusElm.append('<div>Sujet trop court</div>')
+    }
+
+    if(message.length > 20) {
+      statusElm.append('<div>Message valide</div>')
+    } else {
+      statusElm.append('<div>Message trop court</div>')
+    }
+  })
+
+})
