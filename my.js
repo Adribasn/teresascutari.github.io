07461e6b960cf829fe39859dbdac9a571de71dd9
Teresa Scutari <adrien.basyn@outlook.com>
2020-12-16 13:58:04 +0100

    last commit

--- my.js
+++ my.js
@@ -1,15 +1,6 @@
 // Smooth scroll
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
+$(document).ready(function () {
+  $("html, body").animate({
+      scrollTop: $("#div5").offset().top
+  }, 2000);
+});
